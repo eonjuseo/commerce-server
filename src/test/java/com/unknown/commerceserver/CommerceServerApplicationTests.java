7af59a51c137e379149212fd08038bdbe6b4e15b
Seo Eonju <sksjang12@gmail.com>
2024-06-18 13:23:52 +0900

    init

---
+++ src/test/java/com/unknown/commerceserver/CommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unknown.commerceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
